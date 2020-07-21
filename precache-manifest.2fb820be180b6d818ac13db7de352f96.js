e993e593533eafe48d1bf383e0905557d6515ac4
”Ivan <bohdan.dz.92@gmail.com>
2020-07-21 12:32:07 +0300

    Updates

---
+++ precache-manifest.2fb820be180b6d818ac13db7de352f96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c24661e677fb19861b5934c431ff43",
+    "url": "/http//dziubliukbm.github.io/test/index.html"
+  },
+  {
+    "revision": "387e9b25903078808b6c",
+    "url": "/http//dziubliukbm.github.io/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb10e1f916e0684e3bdf",
+    "url": "/http//dziubliukbm.github.io/test/static/js/2.e290e6f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//dziubliukbm.github.io/test/static/js/2.e290e6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387e9b25903078808b6c",
+    "url": "/http//dziubliukbm.github.io/test/static/js/main.75342c00.chunk.js"
+  },
+  {
+    "revision": "c916ebb0bd1f195fba8d",
+    "url": "/http//dziubliukbm.github.io/test/static/js/runtime-main.39394f35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/http//dziubliukbm.github.io/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
